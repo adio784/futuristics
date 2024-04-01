5e004edb24dd2feec1e3fc780960f7d90323a869
kobiowuquadri <kobiowuq@gmail.com>
2024-04-01 09:45:44 -0500

    fix bug: dashboard protection issue

--- src/Utils/ProtectedUserRoutes.jsx
+++ src/Utils/ProtectedUserRoutes.jsx
@@ -2,10 +2,10 @@ import React, {useState, useEffect} from 'react'
 import { Outlet, Navigate } from 'react-router-dom'
 
 function ProtectedUserRoutes() {
-    const [isUser, setIsUser] = useState(JSON.parse(localStorage.getItem('userToken')));
+    const [isUser, setIsUser] = useState(JSON.parse(localStorage.getItem('loginToken')));
 
     useEffect(() => {
-      setIsUser(JSON.parse(localStorage.getItem('userToken')));
+      setIsUser(JSON.parse(localStorage.getItem('loginToken')));
     }, []);
   
     return isUser ? <Outlet /> : <Navigate to='/' />;
